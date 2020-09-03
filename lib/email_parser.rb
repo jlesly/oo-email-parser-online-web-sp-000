e05e0679537b8b528522768ad51e07e455a87cd3
Lesly Juarez <leslyj2@gmail.com>
2020-09-03 17:45:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,18 +10,3 @@ def initialize(parse)
   @parse = parse
 end 
 
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
-
-end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailAddressParser.new(emails2)
-parser2.parse
